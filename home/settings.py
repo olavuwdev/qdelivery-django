54c0aa57fee0091319a6c6240ff985f66d5ddcd6
Olavo Adriel <ollavoadriel@gmail.com>
2024-08-04 22:50:18 -0300

    passando para o mysql

--- home/settings.py
+++ home/settings.py
@@ -83,8 +83,14 @@ WSGI_APPLICATION = 'home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'ifrnzl',
+        'USER':'ifrnzl',
+        'PASSWORD':'ifrnzl',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
